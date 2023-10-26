88581fb11733c044b541fdecbd1301af4bec1039
William Arnold <will748@gmail.com>
2023-10-26 17:48:39 +0900

    "

--- kate/src/lib.rs
+++ kate/src/lib.rs
@@ -170,6 +170,9 @@ pub mod testnet {
 // To be used for incentivised testnet
 #[cfg(feature = "std")]
 pub mod testnet_v2 {
+	use std::fs::OpenOptions;
+	use std::io::{Cursor, Read};
+
 	use super::*;
 	use poly_multiproof::ark_serialize::CanonicalDeserialize;
 	use poly_multiproof::m1_blst;
@@ -180,6 +183,39 @@ pub mod testnet_v2 {
 		// We can also use the raw data to make deserilization faster at the cost of size of the data
 		let pp_bytes = include_bytes!("pp_1024.data");
 		PublicParameters::from_slice(pp_bytes).expect("Deserialization should work")
+	}
+
+	fn read_points(path: &str) -> std::io::Result<(Vec<G1>, Vec<G2>)> {
+		let bytes = include_bytes!("extracted_1024.data");
+		let mut fd = Cursor::new(bytes);
+		const N: usize = 1024;
+
+		let mut hash: [u8; 64] = [0; 64];
+		fd.read_exact(&mut hash[..])?;
+		println!("hash: {}", hex::encode(hash));
+
+		let mut g1s: Vec<u8> = vec![0; 48 * N];
+		for i in 0..N {
+			let start = i * 48;
+			fd.read_exact(&mut g1s[start..(start + 48)])?;
+		}
+
+		let mut g2s: Vec<u8> = vec![0; 96 * 2];
+		for i in 0..2 {
+			let start = i * 96;
+			fd.read_exact(&mut g2s[start..(start + 96)])?;
+		}
+
+		let a: Vec<G1> = g1s
+			.chunks_exact(48)
+			.map(|b| G1::deserialize_compressed(b).unwrap())
+			.collect();
+		let b: Vec<G2> = g2s
+			.chunks_exact(96)
+			.map(|b| G2::deserialize_compressed(b).unwrap())
+			.collect();
+
+		Ok((a, b))
 	}
 
 	// Loads the pre-generated trusted g1 & g2 from the file
@@ -222,51 +258,111 @@ pub mod testnet_v2 {
 
 	#[cfg(test)]
 	mod tests {
-		use core::marker::PhantomData;
 
 		use super::*;
-		use dusk_bytes::Serializable;
 		use dusk_plonk::{
-			fft::{EvaluationDomain as PlonkED, Evaluations as PlonkEV},
-			prelude::BlsScalar,
+			commitment_scheme::kzg10::proof::Proof,
+			fft::{EvaluationDomain as DPEvaluationDomain, Evaluations},
+		};
+		use kate_recovery::{data::Cell, matrix::Position};
+		use pmp::{
+			ark_poly::{
+				univariate::DensePolynomial, DenseUVPolynomial, EvaluationDomain,
+				GeneralEvaluationDomain,
+			},
+			traits::KZGProof,
 		};
 		use poly_multiproof::{
-			ark_ff::{BigInt, Fp},
-			ark_poly::{EvaluationDomain, GeneralEvaluationDomain},
-			ark_serialize::CanonicalSerialize,
 			m1_blst::Fr,
-			traits::Committer,
+			traits::{AsBytes, Committer},
 		};
 		use rand::thread_rng;
 
 		#[test]
 		fn test_consistent_testnet_params() {
-			let pmp = multiproof_params();
-			let dp_evals = (0..30)
-				.map(|_| BlsScalar::random(&mut thread_rng()))
-				.collect::<Vec<_>>();
+			let pmp = testnet_v2::multiproof_params();
+			let pmp2 = testnet_v2::public_params();
 
-			let pmp_evals = dp_evals
+			let points = DensePolynomial::<Fr>::rand(1023, &mut thread_rng()).coeffs;
+			let points2: Vec<_> = points
 				.iter()
-				.map(|i| Fp(BigInt(i.0), PhantomData))
-				.collect::<Vec<Fr>>();
+				.map(|p| BlsScalar::from_bytes(&p.to_bytes().unwrap()).unwrap())
+				.collect();
 
-			let dp_poly =
-				PlonkEV::from_vec_and_domain(dp_evals, PlonkED::new(1024).unwrap()).interpolate();
+			let dp_ev = DPEvaluationDomain::new(1024).unwrap();
+			let dp_poly = Evaluations::from_vec_and_domain(points2.clone(), dp_ev).interpolate();
+			let dp_domain_pts = dp_ev.elements().collect::<Vec<_>>();
 			let pmp_ev = GeneralEvaluationDomain::<Fr>::new(1024).unwrap();
-			let pmp_poly = pmp_ev.ifft(&pmp_evals);
+			let pmp_poly = pmp_ev.ifft(&points);
+			let pmp_domain_pts = pmp_ev.elements().collect::<Vec<_>>();
 
-			let pubs = testnet_v2::public_params();
+			let dp_commit = pmp2.commit_key().commit(&dp_poly).unwrap();
+			let pmp_commit = pmp.commit(&pmp_poly).unwrap();
 
-			let dp_commit = pubs.commit_key().commit(&dp_poly).unwrap().0.to_bytes();
-			let mut pmp_commit = [0u8; 48];
-			pmp.commit(pmp_poly)
-				.unwrap()
-				.0
-				.serialize_compressed(&mut pmp_commit[..])
+			assert_eq!(dp_commit.0.to_bytes(), pmp_commit.to_bytes().unwrap());
+
+			let proof = pmp
+				.open(
+					pmp.compute_witness_polynomial(pmp_poly, pmp_domain_pts[1])
+						.unwrap(),
+				)
 				.unwrap();
 
-			assert_eq!(dp_commit, pmp_commit);
+			let proof2 = pmp2
+				.commit_key()
+				.commit(
+					&pmp2
+						.commit_key()
+						.compute_single_witness(&dp_poly, &dp_domain_pts[1]),
+				)
+				.unwrap();
+
+			assert_eq!(proof.to_bytes().unwrap(), proof2.to_bytes());
+
+			let verify1 = pmp
+				.verify(&pmp_commit, pmp_domain_pts[1], points[1], &proof)
+				.unwrap();
+
+			let dp_proof_obj = Proof {
+				commitment_to_witness: proof2,
+				evaluated_point: points2[1],
+				commitment_to_polynomial: dp_commit,
+			};
+			assert!(pmp2.opening_key().check(dp_domain_pts[1], dp_proof_obj));
+
+			let mut content = [0u8; 80];
+			content[..48].copy_from_slice(&proof2.to_bytes());
+			content[48..].copy_from_slice(&points2[1].to_bytes());
+			let verify2 = kate_recovery::proof::verify(
+				&pmp2,
+				Dimensions::new(1, 1024).unwrap(),
+				&dp_commit.0.to_bytes(),
+				&Cell {
+					content,
+					position: Position { row: 0, col: 1 },
+				},
+			)
+			.unwrap();
+
+			assert!(verify1);
+			assert!(verify2);
+
+			let pmp2_points = read_points("kate/src/extracted_1024.data").unwrap();
+			let pmp2 = m1_blst::M1NoPrecomp::new_from_powers(pmp2_points.0, pmp2_points.1);
+
+			let evals = DensePolynomial::<Fr>::rand(1023, &mut thread_rng()).coeffs;
+			let domain = GeneralEvaluationDomain::<Fr>::new(1024).unwrap();
+			let poly = domain.ifft(&evals);
+
+			let cm1 = pmp.commit(&poly).unwrap();
+			let cm2 = pmp2.commit(&poly).unwrap();
+			assert_eq!(cm1.0, cm2.0);
+
+			let point = gridgen::domain_points(4).unwrap()[1];
+			let witness = pmp.compute_witness_polynomial(poly, point).unwrap();
+			let proof = pmp.open(witness).unwrap();
+
+			assert!(pmp.verify(&cm1, point, evals[1], &proof).unwrap());
 		}
 	}
 }
