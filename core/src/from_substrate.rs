fc9b1b06a35a8ab9fdcfaa6a60d0ae6678148cc6
Marko Petrlic <petrlic.marko@gmail.com>
2024-10-15 13:09:34 +0200

    Fixed clippy screaming at me

--- core/src/from_substrate.rs
+++ core/src/from_substrate.rs
@@ -34,7 +34,8 @@ pub struct HexDisplay<'a>(pub &'a [u8]);
 
 impl<'a> sp_std::fmt::Display for HexDisplay<'a> {
 	fn fmt(&self, f: &mut sp_std::fmt::Formatter) -> Result<(), sp_std::fmt::Error> {
-		if self.0.len() < 1027 {
+		let len = self.0.len();
+		if len < 1027 {
 			for byte in self.0 {
 				f.write_fmt(format_args!("{:02x}", byte))?;
 			}
@@ -43,7 +44,8 @@ impl<'a> sp_std::fmt::Display for HexDisplay<'a> {
 				f.write_fmt(format_args!("{:02x}", byte))?;
 			}
 			f.write_str("...")?;
-			for byte in &self.0[self.0.len() - 512..] {
+			let start = len.saturating_sub(512);
+			for byte in &self.0[start..] {
 				f.write_fmt(format_args!("{:02x}", byte))?;
 			}
 		}
