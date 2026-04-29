044d045f919119206db4dccadab57e8d6890769e
Toufeeq Pasha <toufeeqpasha.2014@gmail.com>
2026-04-29 11:34:40 +0530

    Make FriCommitment fields camelCase

--- core/src/header/extension/fri_v1.rs
+++ core/src/header/extension/fri_v1.rs
@@ -12,6 +12,7 @@ use sp_debug_derive::RuntimeDebug;
 /// Metadata needed for DA sampling + PCS verification of one blob.
 #[derive(Clone, PartialEq, Eq, Encode, Decode, DecodeWithMemTracking, Default, TypeInfo)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "serde", serde(rename_all = "camelCase"))]
 #[cfg_attr(feature = "runtime", derive(RuntimeDebug))]
 pub struct FriBlobCommitment {
 	/// Blob hash
